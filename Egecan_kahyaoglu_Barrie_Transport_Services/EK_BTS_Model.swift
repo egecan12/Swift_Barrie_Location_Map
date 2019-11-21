26ad24fde65d20597ebff27a3d3a9faaa96f49c5
egecan <egecan18k@gmail.com>
2019-11-20 21:12:18 -0500

    first c

---
+++ Egecan_kahyaoglu_Barrie_Transport_Services/EK_BTS_Model.swift
@@ -0,0 +1,31 @@
+//
+//  EK_BTS_Model.swift
+//  Egecan_kahyaoglu_Barrie_Transport_Services
+//
+//  Created by Egecan Kahyaoglu on 2019-11-20.
+//  Copyright © 2019 Egecan Kahyaoglu. All rights reserved.
+//
+
+import Foundation
+
+class EK_BTS_Model{
+    
+    var drivers = [
+         "Alex" : "64755500487",
+         "Alisha" : "647564313204",
+         "Carlos" : "64755587004",
+         "Melda" : "689755004",
+         "Lucio" : "7055236004",
+        "Garry" : "7055556004"
+    
+    ]
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
